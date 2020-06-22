e7728716747c291334f777310633085372a0eaf3
Vishal Viniyadas Abraham <42376781+Appu13@users.noreply.github.com>
2020-06-22 21:11:51 +0530

    Add files via upload
    This is a program to add two numbers in C++

---
+++ APlusB.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int sum_of_two_digits(int first_digit, int second_digit)
+{
+    //Returning output
+    return first_digit + second_digit;
+}
+
+int main() {
+    int a = 0;
+    int b = 0;
+
+    //Receiving the input
+    std::cin >> a;
+    std::cin >> b;
+
+    //Displaying output
+    std::cout << sum_of_two_digits(a, b);
+    return 0;
+}//By appu13
