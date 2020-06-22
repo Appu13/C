e503def12022c1c908a3b1b65b6056b67a4cf38b
Vishal Viniyadas Abraham <42376781+Appu13@users.noreply.github.com>
2020-06-22 21:14:46 +0530

    Add files via upload
    Programm to find the maximum product in an array.
    It works on the logic that the maximum product will be the product of the highest and the second-highest number in the array.

---
+++ max_pairwise_product.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<cstdlib>
+#include<bits/stdc++.h>
+
+
+long long MaxPairwiseProduct_fast (std:: vector<int>&numbers)
+
+{
+    int n=numbers.size();
+    //Sorting the vector
+    sort(numbers.begin(),numbers.end());
+    //Returning the product of last two elements
+    return (long long)numbers[n-1]*numbers[n-2];
+
+}
+
+int main() {
+    int n1;
+
+    // Receiving input
+    std::cin >> n1;
+    std::vector<int> numbers(n1);
+    for (int i = 0; i < n1; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    //Display output
+    std::cout << MaxPairwiseProduct_fast(numbers)<< "\n";
+    return 0;
+}//by appu13
