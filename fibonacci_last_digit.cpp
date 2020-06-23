46999e3a2011dbc815fcfcc943bd310048ab4244
Vishal Viniyadas Abraham <42376781+Appu13@users.noreply.github.com>
2020-06-23 20:46:09 +0530

    Add files via upload
    This is a program to find the last digit of the nth Fibonacci number.

---
+++ fibonacci_last_digit.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+//Naive apporach
+int get_fibonacci_last_digit_naive(int n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current  = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+    }
+
+    return current % 10;
+}
+
+//Fast apporach
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current  = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous;
+        previous = current;
+        current = (tmp_previous + current)%10;
+    }
+
+    return current;
+}
+
+
+
+int main() {
+    int n;
+
+    //Input the n value
+    std::cin >> n;
+
+    //Displaying the value
+    std::cout << get_fibonacci_last_digit_fast(n) << '\n';
+
+}//By appu13
+
+
+
