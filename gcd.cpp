c9efb5e2b7d9f645cdb1cedb771a8ff83a0343cd
Vishal Viniyadas Abraham <42376781+Appu13@users.noreply.github.com>
2020-06-23 20:47:58 +0530

    Add files via upload
    This is a program to find the greatest common divisor of two numbers.

---
+++ gcd.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+//Naive apporach
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+
+  return current_gcd;
+}
+
+//Fast apporach
+int gcd_fast(int a, int b)
+{
+     while(a!=b)
+     {
+         if(a>b)
+            a-=b;
+         else
+            b-=a;
+     }
+
+     return a;
+}
+
+
+
+int main() {
+  int a, b;
+
+  //Input the value
+ std::cin>>a>>b;
+
+ //Display the value
+ std::cout<<gcd_fast(a,b);
+  return 0;
+}///By appu13
